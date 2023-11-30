c5a80e503023c54c175d4e632ce023a723a83794
aniqkcuse <kencasique02@gmail.com>
2023-11-30 09:01:19 -0400

    Serializer the data

---
+++ serializers.py
@@ -0,0 +1,18 @@
+from app import ma
+from models import User, Comments, Topic, Discussion
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+
+class CommentsSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Comments
+
+class TopicSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Topic
+
+class DiscussionSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Discussion

e80ca6cad934e9c5ff4778c6dec5244c6bc0546a
aniqkcuse <kencasique02@gmail.com>
2023-12-02 17:31:46 -0400

    Added the left rows

--- models.py
+++ models.py
@@ -8,16 +8,17 @@ class User(db.Model):
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(1000), nullable=False)
-    author = db.Column(db.ForeignKey("user.id"))
-    forum_id = db.Column(db.ForeignKey("discussion.id"))
+    author = db.Column(db.Integer, db.ForeignKey("user.id"))
+    forum_id = db.Column(db.Integer, db.ForeignKey("discussion.id"))
 
 class Topic(db.Model):
-    name = db.Column(db.String(20), primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
 
 class Discussion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    topic = db.Column(db.String(20), db.ForeignKey("topic.name"))
+    topic = db.Column(db.Integer, db.ForeignKey("topic.id"))
     title = db.Column(db.String(40), nullable=False)
     description = db.Column(db.String(1000), nullable=False)
     answer = db.relationship('Comments', backref='answer')
-    author = db.Column(db.ForeignKey("user.id"))
+    author = db.Column(db.Integer, db.ForeignKey("user.id"))

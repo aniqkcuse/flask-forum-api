0d76426994a54f80f4feced055900e5b45870705
aniqkcuse <kencasique02@gmail.com>
2023-12-02 11:35:03 -0400

    New file to create the app object

---
+++ config_app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from flask_restful import Api
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+api = Api(app)
+migrate = Migrate(app, db)

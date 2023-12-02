261993f661094de0469293de511f8766e168b372
aniqkcuse <kencasique02@gmail.com>
2023-12-02 11:34:08 -0400

    User CRUD created

--- app.py
+++ app.py
@@ -1,7 +1,68 @@
-from flask import Flask
+from models import User, Comments, Topic, Discussion, db
+from serializers import UserSchema, CommentsSchema, TopicSchema, DiscussionSchema, ma
+from config_app import app, api
+from flask import request
+from flask_restful import Resource, reqparse
 
-app = Flask(__name__)
+#Testing data
+with app.app_context():
+    db.create_all()
+    username_schema = UserSchema()
+    topic_schema = TopicSchema()
+    discussion_schema = DiscussionSchema()
+    comment_schema = CommentsSchema()
 
-@app.get("/api/v1/discussion/", method=["GET","POST"])
-def get_discussion():
-    return "<h1>Hello world</h1>"
+    user_parser = reqparse.RequestParser()
+    user_parser.add_argument('username', required=True)
+    user_parser.add_argument('email', required=True)
+
+    class UserAllData(Resource):
+        def get(self):
+            user = db.session.get(User, 3)
+            return username_schema.dump(user)
+        
+        def post(self):
+            args = user_parser.parse_args()
+            user = User(username=args["username"], email=args["email"])
+            db.session.add(user)
+            db.session.commit()
+            return {"msg":"User created"}
+
+    class UserData(Resource):
+        def get(self, pk):
+            user = db.session.get(User, pk)
+            return username_schema.dump(user)
+
+        def put(self, pk):
+            args = user_parser.parse_args()
+            user = db.session.get(User, pk)
+            user.username = args["username"]
+            user.email = args["email"]
+            db.session.add(user)
+            db.session.commit()
+            return {"msg":"User updated"}
+
+        def delete(self, pk):
+            user = db.session.get(User, pk)
+            db.session.delete(user)
+            db.session.commit()
+            return {"msg":"User deleted"}
+
+    api.add_resource(UserAllData, "/api/v1/user/")
+    api.add_resource(UserData, "/api/v1/user/<int:pk>/")
+
+    @app.get("/api/v1/topic/")
+    def get_all_topic():
+        topic = db.session.get(Topic, "TopicOne")
+        return topic_schema.dump(topic)
+
+    @app.get("/api/v1/discussion/")
+    def get_all_discussion():
+        discussion = db.session.get(Discussion, 1)
+        return discussion_schema.dump(discussion)
+
+    @app.get("/api/v1/comment/")
+    def get_all_comment():
+        comment = db.session.get(Comments, 1)
+        return comment_schema.dump(comment)
+
